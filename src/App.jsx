eab4f523563e14e9d1fa8b18a11cad670734dcca
Alejandro Castro <alejandro.castro@unac.edu.co>
2024-09-10 14:57:32 -0500

    modified components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import LoginMain from './pages/LoginMain'
 import Index from './pages/Index'
+import Register from './pages/Register'
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Index />} />  
       <Route path="/Login" element={<LoginMain />} />  
+      <Route path='/register' element={<Register/>}/>
     </Routes>
   )
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -5,15 +5,15 @@ import '../../src/Styles/Login.css'
 import CardImg from '../components/CardImg'
 import Buttons from '../components/Buttons'
 
-function Login() {
+function Register() {
   return (
     <>
       <div className="container-main">
         <CardImg/>
         <div className="container-title">
           <img src={Logo} alt="logo" className='logo'/>
-          <h1>Log in to your Account</h1>
-          <h6>Select method to log in:</h6>
+          <h1>Register</h1>
+          <h6>Select method to Register:</h6>
           <Buttons/>
           <div className="container-line">
             <div className="line1"></div>
@@ -21,17 +21,13 @@ function Login() {
             <div className="line2"></div>
           </div>
           <div className="container-form">
+            <input type="text" placeholder='Username' className='input'/>
             <input type="text" placeholder='Gmail or Username' className='input'/>
             <input type="password" placeholder='Password' className='input'/>
             <div className="container-remember">
-            <label className='remember' >
-              <input type="checkbox" className="remember-me" name="remember-me"/>
-                Recuérdame
-            </label>
-              <Link rel="stylesheet" href="" className='forgot'>Forgot your password?</Link>
             </div>
-            <Link className='btn-login'>Log in</Link>
-            <Link rel="stylesheet" to='/register' className='forgot'>Don’t have account? Create an account</Link>
+            <Link className='btn-login'>Sing in</Link>
+            <Link rel="stylesheet" to='/Login' className='forgot'>You have a account?</Link>
           </div>
         </div>
       </div>
@@ -39,4 +35,4 @@ function Login() {
   )
 }
 
-export default Login
+export default Register
