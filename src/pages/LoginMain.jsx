1648996ae0fab3fdb217d9590277d0179c6eecef
Alejandro Castro <alejandro.castro@unac.edu.co>
2024-09-27 13:37:14 -0500

    Changes

--- src/pages/LoginMain.jsx
+++ src/pages/LoginMain.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom'
 import Logo from '../../src/assets/logo.png'
 import '../../src/Styles/Login.css'
 import CardImg from '../components/CardImg'
-import Buttons from '../components/Buttons'
 
 function Login() {
   return (
@@ -13,20 +12,14 @@ function Login() {
         <div className="container-title">
           <img src={Logo} alt="logo" className='logo'/>
           <h1>Log in to your Account</h1>
-          <h6>Select method to log in:</h6>
-          <Buttons/>
-          <div className="container-line">
-            <div className="line1"></div>
-            <h6>or continue with email</h6>
-            <div className="line2"></div>
-          </div>
+          <h6>continue with email:</h6>
           <div className="container-form">
             <input type="text" placeholder='Gmail or Username' className='input'/>
             <input type="password" placeholder='Password' className='input'/>
             <div className="container-remember">
             <label className='remember' >
               <input type="checkbox" className="remember-me" name="remember-me"/>
-                Recuérdame
+                Rememeber me 
             </label>
               <Link rel="stylesheet" href="" className='forgot'>Forgot your password?</Link>
             </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,35 +3,29 @@ import { Link } from 'react-router-dom'
 import Logo from '../../src/assets/logo.png'
 import '../../src/Styles/Login.css'
 import CardImg from '../components/CardImg'
-import Buttons from '../components/Buttons'
+
 
 function Register() {
   return (
     <>
-      <div className="container-main">
-        <CardImg/>
-        <div className="container-title">
-          <img src={Logo} alt="logo" className='logo'/>
-          <h1>Register</h1>
-          <h6>Select method to Register:</h6>
-          <Buttons/>
-          <div className="container-line">
-            <div className="line1"></div>
-            <h6>or continue with email</h6>
-            <div className="line2"></div>
+    <div className="container-main">
+      <CardImg/>
+      <div className="container-title">
+        <img src={Logo} alt="logo" className='logo'/>
+        <h1>Register your account</h1>
+        <h6>Continue with your registration:</h6>
+        <div className="container-form">
+          <input type="text" placeholder='Username' className='input'/>
+          <input type="text" placeholder='Gmail' className='input'/>
+          <input type="password" placeholder='Password' className='input'/>
+          <div className="container-remember">
           </div>
-          <div className="container-form">
-            <input type="text" placeholder='Username' className='input'/>
-            <input type="text" placeholder='Gmail or Username' className='input'/>
-            <input type="password" placeholder='Password' className='input'/>
-            <div className="container-remember">
-            </div>
-            <Link className='btn-login'>Sing in</Link>
+          <Link className='btn-login'>Sing in</Link>
             <Link rel="stylesheet" to='/Login' className='forgot'>You have a account?</Link>
-          </div>
         </div>
       </div>
-    </>
+    </div>
+  </>
   )
 }
 
